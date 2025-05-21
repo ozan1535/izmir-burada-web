188123a2e551e5f922590c83c044a0a4f43967e0
ozan1535 <ozanbilgic1535@gmail.com>
2025-05-21 14:45:03 +0300

    Use Link instead of a tag

--- src/components/Header/NavbarItems/NavbarItems.tsx
+++ src/components/Header/NavbarItems/NavbarItems.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useParams, usePathname } from "next/navigation";
+import Link from "next/link";
 import { navbarItems } from "../helpers";
 
 function NavbarItems() {
@@ -10,7 +11,7 @@ function NavbarItems() {
     <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border md:border-gray-100 rounded-lg md:bg-transparent md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 bg-gray-800 border-gray-700">
       {navbarItems.map((item) => (
         <li key={item.id}>
-          <a
+          <Link
             href={item.href}
             className={`block py-2 px-3 text-lg ${
               pathname === item.href ? "text-sky-400" : "text-white"
@@ -18,7 +19,7 @@ function NavbarItems() {
             aria-current="page"
           >
             {item.name[locale]}
-          </a>
+          </Link>
         </li>
       ))}
     </ul>
